6e8d45221e842c767b62219ef4218a5ee2132162
zed <hoksopheathireach18@kit.edu.kh>
2024-02-05 13:43:12 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe(({ transform: true })));
+  app.enableCors({
+    //Add your origins here
+    origin: true,
+  });
   await app.listen(3000);
+
 }
 bootstrap();
